2aa6f437bb9c2ec1e33e0d0d2b576f55970334a3
Tkachev.Nikita <n.o.tkachev@urfu.ru>
2024-02-08 15:41:57 +0500

    feat: дописал отображение иконок по свойству name

--- projects/urfumaterial/src/lib/components/icon/icon.component.ts
+++ projects/urfumaterial/src/lib/components/icon/icon.component.ts
@@ -7,23 +7,5 @@ import {IconService} from "./icon.serivce";
     styleUrls: ['./icon.component.scss']
 })
 export class UIconComponent {
-    // @Input() name: string = '';
-    // iconSvg: string = '';
-
-    // constructor(private _iconService: IconService) {
-    //
-    // }
-
-    // ngOnInit() {
-    //     this.loadIcon();
-    // }
-    //
-    // private loadIcon() {
-    //     if (this.name) {
-    //         this._iconService.getSvgContent(this.name)
-    //             .subscribe(svgContent =>
-    //                 this.iconSvg = svgContent
-    //             );
-    //     }
-    // }
+    @Input() name!: string;
 }
