1707b585888a8a74585718fe7227e9b73bd8a1d6
Tkachev.Nikita <n.o.tkachev@urfu.ru>
2024-06-25 17:49:09 +0500

    доделал страницу кнопок

--- src/app/pages/button-page/button-page.component.ts
+++ src/app/pages/button-page/button-page.component.ts
@@ -1,12 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, input, InputSignal} from "@angular/core";
 import {UButtonModule} from "urfumaterial";
+import {CodeComponent} from "../../components/code/code.component";
+import {HighlightModule} from "ngx-highlightjs";
+import {HighlightLineNumbers} from "ngx-highlightjs/line-numbers";
 
 @Component({
     templateUrl: './button-page.component.html',
     styleUrls: ['./button-page.component.scss'],
     standalone: true,
     imports: [
-        UButtonModule
+        UButtonModule,
+        CodeComponent,
+        HighlightModule,
+        HighlightLineNumbers
     ]
 })
 export class ButtonPageComponent {
@@ -14,4 +20,13 @@ export class ButtonPageComponent {
         console.log(1);
     }
 
+
+    code: string = `    <div class="container">
+        <u-button basic variant="primary" (click)="onClick()">Подать заявку</u-button>
+        <u-button basic variant="simple" (click)="onClick()">Подать заявку</u-button>
+        <u-button outlined variant="primary" (click)="onClick()">Подать заявку</u-button>
+        <u-button outlined variant="simple" (click)="onClick()">Подать заявку</u-button>
+        <u-button basic variant="danger" (click)="onClick()">Подать заявку</u-button>
+        <u-button basic disabled variant="danger" (click)="onClick()">Подать заявку</u-button>
+    </div>`;
 }
